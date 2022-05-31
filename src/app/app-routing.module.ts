399ec9ab831354142ac3443777b586719ae7e7b5
igorGavr <ragamuffinrogi@gmail.com>
2022-05-31 10:57:31 +0300

    project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {MainLayoutComponent} from "./layouts/main-layout/main-layout.component";
+import {MoviesComponent} from "./components/movies/movies.component";
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'movies', pathMatch: 'full'},
+      {path: 'movies', component: MoviesComponent}
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {RouterModule} from "@angular/router";
-import { MainLayoutComponent } from './layouts/main-layout/main-layout/main-layout.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import {MainLayoutComponent} from "./layouts/main-layout/main-layout.component";
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { MoviesComponent } from './components/movies/movies.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
